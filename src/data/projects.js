d1516078a5b5cc13a8a6fbca85aecfbe95a91f0f
milendrakumarbaghel <baghelmilendrakumar@gmail.com>
2025-03-25 00:32:52 +0530

    project categories updated

--- src/data/projects.js
+++ src/data/projects.js
@@ -5,10 +5,10 @@ export const projects = [
       category: 'Web Applications',
       image: 'https://images.unsplash.com/photo-1551288049-bebda4e38f71?w=800&h=600&q=80'
     },
-    {
-      id: 2,
-      title: 'Brawlhalla',
-      category: 'Mobile Applications',
-      image: 'https://images.unsplash.com/photo-1556438064-2d7646166914?w=800&h=600&q=80'
-    }
+    // {
+    //   id: 2,
+    //   title: 'Brawlhalla',
+    //   category: 'Mobile Applications',
+    //   image: 'https://images.unsplash.com/photo-1556438064-2d7646166914?w=800&h=600&q=80'
+    // }
   ];
